f52030c10885f5b3872fe34e8bc0dac394e4e09d
maribelcuales <maribelcuales@gmail.com>
2020-06-18 16:23:06 +0800

    Added tests for inputs name and email.

--- cypress/integration/examples/form_test.js
+++ cypress/integration/examples/form_test.js
@@ -5,5 +5,9 @@ describe("Testing our volunteer form", function() {
   it("Add test to inputs and submit form", function() {
     cy.get('input[name="name"]')
       .type("Christina")
+      .should("have.value", "Christina");
+    cy.get('input[name="email"]')
+      .type("email@email.com")
+      .should("have.value", "email@email.com");
   });
 });
